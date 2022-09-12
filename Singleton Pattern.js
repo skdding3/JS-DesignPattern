5d1f97fd183370f15b06a8eaf44763941058fbf6
skdding3 <skdding3@gmail.com>
2022-09-12 21:53:29 +0900

    DP : Singleton Pattern

--- Singleton Pattern.js
+++ Singleton Pattern.js
@@ -1,20 +1,58 @@
 // # Singleton Pattern
 
+// Cat
 
-class Cat {
+// class Cat {
+//     constructor(name) {
+//         this.name = name
+//     }
+//     speak() {
+//         console.log(this.name, 'meow')
+//     }
+// }
+//
+//
+// let kitty = new Cat('kitty')
+// let nabi = new Cat('nabi')
+// //
+// // kitty.speak();
+// // nabi.speak();
+//
+// if (kitty === nabi) {
+//     console.log('they are same')
+// } else {
+//     console.log('they are not same')
+// }
+
+
+class Singleton {
+    static instance;
     constructor(name) {
         this.name = name
+        if(!Singleton.instance) {
+            Singleton.instance = this;
+        }
+        return Singleton.instance;
     }
+
     speak() {
-        console.log(this.name, 'meow')
+        console.log(this.name, '미야옹')
     }
 }
 
-let kitty = new Cat('kitty')
-let nabi = new Cat('nabi')
+// let s1 = new Singleton();
+// let s2 = new Singleton();
+//
+// if (s1 === s2) {
+//     console.log('그들은 같아요, 싱글톤!')
+// } else {
+//     console.log('그들은 같지 않아요')
+// }
 
-if (kitty === nabi) {
-    console.log('they are same')
-} else {
-    console.log('they are not same')
-}
+let sKitty = new Singleton('kitty')
+let sNabi = new Singleton('nabi')
+
+sKitty.speak(); // kitty 미야옹
+sNabi.speak(); // kitty 미야옹
+
+// 싱글톤 주요 요점은 단 하나의 오브젝트만 생성한다. 그 오브젝트를 통해 여러 변수가 돌아가서 프로세스에 하나의 리소스만 차지한다.
